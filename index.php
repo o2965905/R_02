93188f8e8e5102487a68f5e77609bf11c4200c69
o2965905 <o2965905@gmail.com>
2022-09-07 14:28:07 +0800

    五.建置動態文字廣告區

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ include_once "./base.php";
 			</div>
 			<div class="hal" id="main">
 				<div>
-
+					<marquee style="width:80%;display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地!詳見最新文章。</marquee>
 					<span style="width:18%; display:inline-block;">
 						<a href="?do=login">會員登入</a>
 					</span>
