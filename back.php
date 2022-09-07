60670840438423d99c1dd9f41049c8319e7d797a
o2965905 <o2965905@gmail.com>
2022-09-07 14:23:07 +0800

    四.建置主選單區/十四.建置後台管理介面

--- back.php
+++ back.php
@@ -29,11 +29,11 @@ include_once "./base.php";
 		</div>
 		<div id="mm">
 			<div class="hal" id="lef">
+				<a class="blo" href="?do=user">帳號管理</a>
 				<a class="blo" href="?do=po">分類網誌</a>
-				<a class="blo" href="?do=news">最新文章</a>
-				<a class="blo" href="?do=pop">人氣文章</a>
-				<a class="blo" href="?do=know">講座訊息</a>
-				<a class="blo" href="?do=que">問卷調查</a>
+				<a class="blo" href="?do=news">最新文章管理</a>
+				<a class="blo" href="?do=know">講座管理</a>
+				<a class="blo" href="?do=que">問卷管理</a>
 			</div>
 			<div class="hal" id="main">
 				<div>
@@ -44,11 +44,11 @@ include_once "./base.php";
 					<div class="content">
 						<?php
 						$do=$_GET['do']??'main';
-						$file="./front/".$do.".php";
+						$file="./back/".$do.".php";
 						if(file_exists($file)){
 							include $file;
 						}else{
-							include "./front/main.php";
+							include "./back/main.php";
 						}
 						?>
 					</div>
