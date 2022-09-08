59c320487178f7acd215ef7c8a7d96940357573e
o2965905 <o2965905@gmail.com>
2022-09-08 14:55:54 +0800

    十三.建置問卷調查區

---
+++ api/vote.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once "../base.php";
+
+$opt=$_POST['opt'];
+$option=$Que->find($opt);
+$option['count']++;
+$Que->save($option);
+
+$subject=$Que->find($option['subject_id']);
+$subject['count']++;
+$Que->save($subject);
+
+to('../index.php?do=result&id='.$subject['id']);
+
+?>
